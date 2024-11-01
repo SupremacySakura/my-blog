5f1156880f3b05576a669b7357c8a35583bef339
Supremacy <3118654731@qq.com>
2024-11-01 10:46:54 +0800

    0.7.1 优化文章页滚动与展示问题

--- src/views/ArticlesView.vue
+++ src/views/ArticlesView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref,useTemplateRef,watchEffect,nextTick } from 'vue'
+import { onMounted, ref, useTemplateRef, watchEffect, nextTick } from 'vue'
 import test1 from '@/assets/test1.jpg'
 import user from '@/assets/user.png'
 import { getArticles } from '@/services/apis/articles'
@@ -14,7 +14,7 @@ interface iArticleItem {
   digest: string,
   article: string,
   userHeadPortrait: string,
-  name:string,
+  name: string,
   time: string,
   cover: string
 }
@@ -22,7 +22,7 @@ const articlesList = ref<iArticleItem[]>([])
 /**
  * 获取文章数据
  */
-const handleGetArticles =async ()=>{
+const handleGetArticles = async () => {
   const res = await getArticles()
   if (+res.data.code === 200) {
     articlesList.value = res.data.data
@@ -30,7 +30,7 @@ const handleGetArticles =async ()=>{
       if (!item.userHeadPortrait) {
         item.userHeadPortrait = user
       }
-      if(!item.cover){
+      if (!item.cover) {
         item.cover = test1
       }
     })
@@ -39,30 +39,30 @@ const handleGetArticles =async ()=>{
 //右侧展示文章数据
 const articleItem = ref<iArticleItem>()
 const articleMD = useTemplateRef('articleMD')
-watchEffect(async()=>{
-  if(articleMD.value){
-    const htmlContent =await marked(articleItem.value?.article as string)
+watchEffect(async () => {
+  if (articleMD.value) {
+    const htmlContent = await marked(articleItem.value?.article as string)
     articleMD.value.innerHTML = htmlContent
-  }else{
+  } else {
 
   }
 })
-const handleChooseArticle = (item:iArticleItem)=>{
+const handleChooseArticle = (item: iArticleItem) => {
   articleItem.value = item
 }
-const handleClose = ()=>{
+const handleClose = () => {
   articleItem.value = undefined
 }
-onMounted(async()=>{
- await handleGetArticles()
-console.log(articlesList.value)
+onMounted(async () => {
+  await handleGetArticles()
+  console.log(articlesList.value)
 })
 </script>
 
 <template>
   <div class="articlesBox">
 
-    <section>
+    <section class="leftSection">
       <section class="card" v-for="item of articlesList" :key="item.id" @click="handleChooseArticle(item)">
         <div class="image">
           <img :src="item.cover" alt="">
@@ -81,7 +81,7 @@ console.log(articlesList.value)
 
     <section class="articleBoard" v-if="articleItem">
       <div class="close">
-        <el-button type="danger" :icon="CloseBold" circle @click="handleClose"/>
+        <el-button type="danger" :icon="CloseBold" circle @click="handleClose" />
       </div>
       <h2>{{ articleItem.head }}</h2>
       <p ref="articleMD"></p>
@@ -93,98 +93,128 @@ console.log(articlesList.value)
 .articlesBox {
   min-width: 750px;
   width: 100%;
-  min-height: 100vh;
-  padding-top: 100px;
+  height: 100vh;
+  padding-top: 90px;
   padding-left: 40px;
   padding-right: 40px;
-  margin-bottom: 60px;
+  padding-bottom: 10px;
   box-sizing: border-box;
   display: flex;
-
-  .card {
-    width: 700px;
-    height: 200px;
-    border-radius: 8px;
-    background-color: aliceblue;
-    display: flex;
-    overflow: hidden;
-    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2),
-      /* 主阴影 */
-      0px 12px 20px rgba(0, 0, 0, 0.15),
-      /* 次阴影 */
-      0px 24px 40px rgba(0, 0, 0, 0.1);
-
-    /* 远处阴影 */
-    transform: scale(1);
-    transition: transform 0.8s ease;
-    margin-bottom: 20px;
-    &:hover{
-      transform: scale(1.1);
-    }
-    .image {
-      width: 400px;
-      height: 100%;
+  overflow: hidden;
+  .leftSection {
+    padding-top: 20px;
+    padding-left: 40px;
+    width: 900px;
+    height: 90%;
+    overflow-y: auto;
+    scrollbar-width: none;
+      /* Firefox */
+      -ms-overflow-style: none;
+      /* IE 和 Edge */
+      &::-webkit-scrollbar {
+          display: none;
+          /* Chrome、Safari、Edge */
+        }
+    .card {
+      width: 700px;
+      height: 200px;
       border-radius: 8px;
+      background-color: aliceblue;
+      display: flex;
+      overflow: hidden;
+      box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2),
+        /* 主阴影 */
+        0px 12px 20px rgba(0, 0, 0, 0.15),
+        /* 次阴影 */
+        0px 24px 40px rgba(0, 0, 0, 0.1);
 
-      img {
-        width: 100%;
+      /* 远处阴影 */
+      transform: scale(1);
+      transition: transform 0.8s ease;
+      margin-bottom: 20px;
+
+      &:hover {
+        transform: scale(1.1);
+      }
+
+      .image {
+        width: 400px;
         height: 100%;
         border-radius: 8px;
-      }
-    }
-
-    .info {
-      width: 300px;
-      height: 100%;
-      display: flex;
-      flex-direction: column;
-      justify-content: space-around;
-      box-sizing: border-box;
-      padding: 10px;
-
-      .abstract {
-        width: 100%;
-        overflow: hidden;
-        white-space: nowrap;
-        text-overflow: ellipsis;
-      }
-
-      .author {
-        display: flex;
-        align-items: center;
 
         img {
-          width: 50px;
-          height: 50px;
-          border-radius: 50px;
+          width: 100%;
+          height: 100%;
+          border-radius: 8px;
+        }
+      }
+
+      .info {
+        width: 300px;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        box-sizing: border-box;
+        padding: 10px;
+
+        .abstract {
+          width: 100%;
+          overflow: hidden;
+          white-space: nowrap;
+          text-overflow: ellipsis;
         }
 
-        time {
-          color: rgb(166.2, 168.6, 173.4);
-          font-size: 12px;
+        .author {
+          display: flex;
+          align-items: center;
+
+          img {
+            width: 50px;
+            height: 50px;
+            border-radius: 50px;
+          }
+
+          time {
+            color: rgb(166.2, 168.6, 173.4);
+            font-size: 12px;
+          }
         }
       }
     }
   }
-  .articleBoard{
+
+  .articleBoard {
     flex-grow: 1;
-    min-height: 100vh;
-    margin-right:10px;
+    height: 90%;
+    box-sizing: border-box;
+    margin-right: 10px;
     margin-left: 40px;
-    margin-top: 20px;
+    margin-top: 10px;
     padding: 15px;
     background-color: #f7f7f7;
     border-radius: 10px;
     display: flex;
     flex-direction: column;
     align-items: center;
-    .close{
+    overflow: auto;
+      scrollbar-width: none;
+        /* Firefox */
+        -ms-overflow-style: none;
+      
+        /* IE 和 Edge */
+        &::-webkit-scrollbar {
+          display: none;
+          /* Chrome、Safari、Edge */
+        }
+    .close {
       width: 100%;
       display: flex;
       justify-content: end;
       font-size: 20px;
     }
-    p{
+
+    p {
       width: 100%;
     }
   }
