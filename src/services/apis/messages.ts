a15a41956f53ce7c1251a7aab9da0cb146f6e9a0
Supremacy <3118654731@qq.com>
2024-11-03 09:43:39 +0800

    1.4.1 优化发送留言接口

--- src/services/apis/messages.ts
+++ src/services/apis/messages.ts
@@ -3,8 +3,15 @@ import http from '@/services/http'
 const getMessages = () => {
   return http.get('/messages')
 }
+
 const postMessages = (params: any) => {
-  return http.post('/messages/post', { params: params })
+  return http.post('/messages/post', { 
+      userHeadPortrait:params.userHeadPortrait,
+      name:params.name,
+      content:params.content,
+      time:params.time,
+      address:params.address,
+   })
 }
 export {
   getMessages,
